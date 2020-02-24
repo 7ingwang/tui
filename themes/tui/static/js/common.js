502ad110c809d3ca01990fa3e4e972b1af68f2cc
show <Terry@Terrys-MBP.lan>
2020-02-24 20:33:55 +0800

    classList

--- themes/tui/static/js/common.js
+++ themes/tui/static/js/common.js
@@ -1,6 +1,20 @@
-$("#toggle").click(function() {
-  $("#header").toggleClass("open");
-});
-$("#nav li a").click(function() {
-  $("#header").removeClass("open");
-});
+// $("#toggle").click(function() {
+//   $("#header").toggleClass("open");
+// });
+// $("#nav li a").click(function() {
+//   $("#header").removeClass("open");
+// });
+
+
+
+function toggleNav() {
+  document.getElementById("header").classList.toggle("open");
+}
+function removeNav() {
+  document.getElementById("header").classList.remove("open");
+}
+
+
+function toggleSubmenu() {
+  document.getElementsByClassName("submenu")[0].classList.toggle("show");
+}
